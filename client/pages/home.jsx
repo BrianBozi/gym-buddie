475cf9f2603a60750a883df2ba43fa1ee556ca29
Brian Bozigian <bbozigian@outlook.com>
2021-09-03 22:34:35 -0700

    test to make sure the app is working with a h1 and removed it

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+// import { Grid, Card, Paper, Typography } from '@material-ui/core';
 
 export default function Home(props) {
   return (
     <>
+      {/* <Grid container>
+        <Grid item scale={6}>
+          <Typography varient='h1'>
+            <span className="iconify" data-icon="map:gym"></span>
+            GYM BUDDIE
+            </Typography>
+        </Grid>
+        <Grid item>
+
+        </Grid>
+
+      </Grid> */}
     </>
   );
 }
